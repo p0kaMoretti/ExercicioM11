42f0b5389f1d9d7b6e065a805bba4cebb383c3ba
p0ka <p0kkaxd@gmail.com>
2021-11-09 21:30:24 -0300

    Exercicio m11

---
+++ cypress/integration/pre-cadasstro.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+var faker = require('faker');
+
+describe('Funcionalidade pré cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        });
+          
+    afterEach(() => {
+        cy.screenshot()
+    });
+    
+
+    it('Deve completar o pré cadastro com sucesso', () => {
+        let emailFaker = faker.internet.email()
+        let nameFaker = faker.name.firstName()
+        let lastnameFaker = faker.name.lastName()
+
+        cy.get('#reg_email').type(emailFaker)
+        cy.get('#reg_password').type('!pikachus2.')
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nameFaker)
+        cy.get('#account_last_name').type(lastnameFaker)
+        cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+    })
+});
